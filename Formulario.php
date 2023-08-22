738e3fe0f946ac544e2f2bea106da9c04f6e7557
Marktincc <105111683+Marktincc@users.noreply.github.com>
2023-08-22 18:53:08 -0500

    Add files via upload

---
+++ Formulario.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form name="formulario" method="post" action="Prueba.php">
+
+            Nombre: <input type="text" name="nombre" value="">
+            Apellido: <input type="text" name="apellido" value="">
+            Documento: <input type="text" name="documento" value="">
+            Edad: <input type="text" name="edad" value="">
+
+            <input type="submit" />
+
+        </form>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
---
+++ Prueba.php
@@ -0,0 +1,12 @@
+
+<?php
+
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$documento = $_POST['documento'];
+$documento = $_POST['edad'];
+
+
+echo $nombre;
+
+?>
